7e634593c94d6535f01bbd59670650889d23da47
Trần Trung <nguyentrantrung1986@gmail.com>
2016-01-30 19:14:07 -0800

    ClassA1 use "public"

---
+++ AccessOOP/src/GoiA/ClassA1.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package GoiA;
+
+/**
+ *
+ * @author TrungNT
+ */
+public class ClassA1 {
+    public int VarA1 = 5; //bien VarA1 de che do public 
+    
+    public void methodA1(){
+        System.out.println("method A1 trong Class A1 de che do public");
+    }
+}
---
+++ AccessOOP/src/accessoop/AccessOOP.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package accessoop;
+import GoiA.ClassA1;
+
+/**
+ *
+ * @author TrungNT
+ */
+public class AccessOOP {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        ClassA1 oClassA1 = new ClassA1();
+        oClassA1.methodA1();
+    }
+    
+}
